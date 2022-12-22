92ab786e46fffb35a54497bb76453b703cd49948
kubik20083 <kubik20083@mail.ru>
2022-12-22 18:56:00 +0300

    Решение задачи 3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Четное или нечетное число");
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число " + num + " не четное");
+}
+else
+{
+    Console.WriteLine("Число " + num + " четное");
+}
