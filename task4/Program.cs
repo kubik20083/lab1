9157fb733533725b33d558946d05bc1aa5497610
kubik20083 <kubik20083@mail.ru>
2022-12-22 19:16:58 +0300

    Решение задачи 4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int i = 1;
+
+Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + n);
+while (i <= n)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write( i + "\n");
+    }
+      
+      i++;
+}
+
